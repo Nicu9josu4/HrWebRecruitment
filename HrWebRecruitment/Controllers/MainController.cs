cb6f85564134c2fa085f045596ffaaaab49f0485
FCIM-217-PC-2 <FCIM-217-PC-2@PC-2-217>
2025-09-21 10:49:11 +0300

    Set admin page as view instead of html

--- HrWebRecruitment/Controllers/MainController.cs
+++ HrWebRecruitment/Controllers/MainController.cs
@@ -4,7 +4,14 @@ namespace HrWebRecruitment.Controllers
 {
     public class MainController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/Admin")]
+        public IActionResult Admin()
         {
             return View();
         }
